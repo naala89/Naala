dc1469aa237551bb0e3833bbffb907dc1184aa87
naala89 <77126578+naala89@users.noreply.github.com>
2023-04-15 15:29:47 +1000

    Fix responsive footer layout

--- src/components/FooterRegion.vue
+++ src/components/FooterRegion.vue
@@ -2,20 +2,16 @@
   <el-footer>
     <el-row>
 
-      <el-col :xl="3" :lg="0" :md="0" :sm="0" :xs="0">
+      <el-col :xl="{span: 4, offset: 6}"
+              :lg="{span: 4, offset: 6}"
+              :md="{span: 4, offset: 6}"
+              :sm="{span: 4, offset: 6}"
+              :xs="{span: 18, offset: 6}">
+        <h3>Menu</h3>
         <el-menu class="el-menu-vertical-demo"
                  background-color="#000"
                  text-color="#fff"
                  active-text-color="#ffd04b">
-        </el-menu>
-      </el-col>
-
-      <el-col :xl="6" :lg="8" :md="12" :sm="12" :xs="24">
-        <el-menu class="el-menu-vertical-demo"
-                 background-color="#000"
-                 text-color="#fff"
-                 active-text-color="#ffd04b">
-          <h3>Menu</h3>
           <el-menu-item index="1">
             <router-link :to="{ name: 'home' }" custom v-slot="{ href, navigate }">
               <a :href="href" @click="navigate">Home</a>
@@ -49,12 +45,16 @@
         </el-menu>
       </el-col>
 
-      <el-col :xl="6" :lg="8" :md="12" :sm="12" :xs="24">
+      <el-col :xl="{span: 4, offset: 0}"
+              :lg="{span: 4, offset: 0}"
+              :md="{span: 4, offset: 0}"
+              :sm="{span: 4, offset: 0}"
+              :xs="{span: 18, offset: 6}">
+        <h3>Other Links</h3>
         <el-menu class="el-menu-vertical-demo"
                  background-color="#000"
                  text-color="#fff"
                  active-text-color="#ffd04b">
-          <h3>Other Links</h3>
           <el-menu-item index="1">
             <a href="https://www.apiopenstudio.com">ApiOpenStudio</a>
           </el-menu-item>
@@ -64,31 +64,22 @@
           <el-menu-item index="3">
             <a href="https://phpdoc.apiopenstudio.com">ApiOpenStudio PHPDoc</a>
           </el-menu-item>
-          <el-menu-item index="4"></el-menu-item>
-          <el-menu-item index="5"></el-menu-item>
-          <el-menu-item index="6"></el-menu-item>
         </el-menu>
       </el-col>
 
-      <el-col :xl="6" :lg="8" :md="12" :sm="12" :xs="24">
+      <el-col :xl="{span: 4, offset: 0}"
+              :lg="{span: 4, offset: 0}"
+              :md="{span: 4, offset: 0}"
+              :sm="{span: 4, offset: 0}"
+              :xs="{span: 18, offset: 6}">
+        <h3>Contact Us</h3>
         <el-menu class="el-menu-vertical-demo"
                  background-color="#000"
                  text-color="#fff"
                  active-text-color="#ffd04b">
-          <h3>Contact Us</h3>
           <el-menu-item index="2">Email:&nbsp;<a href="mailto:contact@naala.com.au">contact@naala.com.au</a>
           </el-menu-item>
           <el-menu-item index="2">Phone:&nbsp;<a href="tel:+61279083860">+61 (2) 7908 3860</a></el-menu-item>
-          <el-menu-item index="3"></el-menu-item>
-          <el-menu-item index="4">
-            <p>What we're listening to</p>
-          </el-menu-item>
-          <el-menu-item index="5">
-            <iframe src="https://embed.spotify.com/?uri=spotify:track:41X9To0bQ4fpoSRri4NHiI" width="300"
-                    height="80" frameborder="0" allowtransparency="true"></iframe>
-          </el-menu-item>
-          <el-menu-item index="6"></el-menu-item>
-          <el-menu-item index="7"></el-menu-item>
         </el-menu>
       </el-col>
 
